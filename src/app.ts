a2249aa2d5b61c2e1d37a0f962c9274b98b7b06f
Aleisson <caioaleisson@gmail.com>
2022-11-12 22:22:18 -0300

    Docs(dist): create app.ts and config

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
-import express from 'express';
+import express, { json } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+const server = express();
+
+server.use(json());
+
+
+
+server.listen(process.env.PORT, () => {
+    console.log(`PORT ${process.env.PORT}`);
+});
+
