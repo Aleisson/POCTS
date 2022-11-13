a3aa5957af00120c3075643f9cc7abfa0caf059a
Aleisson <caioaleisson@gmail.com>
2022-11-12 22:33:29 -0300

    Docs(app.ts): make route /home for test

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { json } from 'express';
 import dotenv from 'dotenv';
+import { Request, Response } from 'express';
 
 dotenv.config();
 
@@ -8,7 +9,9 @@ const server = express();
 
 server.use(json());
 
-
+server.get('/home', (req: Request, res: Response) => {
+    return res.sendStatus(501);
+})
 
 server.listen(process.env.PORT, () => {
     console.log(`PORT ${process.env.PORT}`);
